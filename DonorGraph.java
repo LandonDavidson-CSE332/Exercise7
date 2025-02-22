3bfe0d7bef188a8a02bba73eb9a652aaa71e19cb
Landon Davidson <accounts@landond.tech>
2025-02-21 17:12:57 -0800

    Fix: Constructor to only make numRecipients LinkedLists

--- DonorGraph.java
+++ DonorGraph.java
@@ -10,15 +10,17 @@ public class DonorGraph {
     // donor-recipient pair. Specificically, matchScores[x][y] gives the
     // HLA score for donor x and reciplient y.
     public DonorGraph(int[] donorToBenefit, int[][] matchScores){
-        adjList = new ArrayList<>();
         // Get number of recipients from matchScores sub array. The first index doesn't matter for size but 0 will always word
         int numRecipients = matchScores[0].length;
         // Create an array of booleans to mark if an edge exists between two recipients in order to keep the graph simple
         boolean[][] edgeExist = new boolean[numRecipients][numRecipients];
+        // Populate adjList with numRecipients LinkedLists
+        adjList = new ArrayList<>();
+        for (int i = 0; i < numRecipients; i++) {
+            adjList.add(new LinkedList<>());
+        }
         // Loop over each donor
         for (int i = 0; i < donorToBenefit.length; i++) {
-            // Add new LinkedList to adjList to store current donor's matches
-            adjList.add(new LinkedList<>());
             // Loop over each of that donors compatability with recipients
             for (int j = 0; j < matchScores[i].length; j++) {
                 // If the donor and beneficiary are compatible and there isn't already an edge add a Match edge object
@@ -26,12 +28,13 @@ public class DonorGraph {
                     // Create an edge with current donor and recipient
                     Match edge = new Match(i, donorToBenefit[i], j);
                     // Add edge to adjList
-                    adjList.get(i).add(edge);
+                    adjList.get(donorToBenefit[i]).add(edge);
                     // Mark the edge as existing
                     edgeExist[donorToBenefit[i]][j] = true;
                 }
             }
         }
+        int tmp = 0;
     }
 
     // Will be used by the autograder to verify your graph's structure.
@@ -59,8 +62,19 @@ public class DonorGraph {
     // which includes the given recipient.
     // Returns an empty list if no cycle exists. 
     public List<Match> findCycle(int recipient){
-        // TODO
-        return new ArrayList<>();
+        // Call the recersive helper to find cycles starting from the intended recipient node
+        return recFindCycle(new LinkedList<>(adjList.get(recipient)), recipient);
+    }
+
+    // Recursive DFS to find a cycle, returns a LinkedList of edges forming the cycle
+    private List<Match> recFindCycle(List<Match> path, int recipient) {
+        int node = 1;
+        int numEdges = 1;
+        for (int i = 0; i < numEdges; i++) {
+
+        }
+        
+        return path;
     }
 
     // returns true or false to indicate whether there
